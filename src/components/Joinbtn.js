6b0f336d1deefcee7371ea9286600012c6dff27f
reitwiec <reitwiec@gmail.com>
2018-11-12 13:28:42 +0530

    Changed the port

--- src/components/Joinbtn.js
+++ src/components/Joinbtn.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
-const lol = () => {console.log("hello")};
+
 class Joinbtn extends Component {
+    lol = () => {console.log("hello")};
     render() {
         return (
             <button id="joinbtn" type="submit" onClick={this.lol}>Join Now</button>
